c150c2a65600f7f68340a1fd07ddf10bd107a8ea
mongro <simon.gross04@web.de>
2025-11-14 22:28:06 +0100

    add neon prisma adapter

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,13 +1,21 @@
 import { PrismaClient } from "./generated/prisma/client";
 //import { withAccelerate } from "@prisma/extension-accelerate";
+import { PrismaNeon } from "@prisma/adapter-neon";
+import dotenv from "dotenv";
 
 const globalForPrisma = global as unknown as {
   prisma: PrismaClient;
 };
 
-const prisma = globalForPrisma.prisma || new PrismaClient();
+dotenv.config();
+const connectionString = `${process.env.DATABASE_URL}`;
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+const adapter = new PrismaNeon({ connectionString });
+const prisma = new PrismaClient({ adapter });
+
+//const prisma = globalForPrisma.prisma || new PrismaClient();
+
+//if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
 export default prisma;
 
