c8121bb7219b9167ea049660fcd0c641cb24a524
mongro <simon.gross04@web.de>
2025-11-14 15:49:40 +0100

    -delete doubled folder

--- components/Tooltip/namespace.tsx
+++
@@ -1,5 +0,0 @@
-import { Tooltip as Root } from "./tooltip";
-import { TooltipContent as Content } from "./tooltip-content";
-import { TooltipTrigger as Trigger } from "./tooltip-trigger";
-
-export { Root, Content, Trigger };
